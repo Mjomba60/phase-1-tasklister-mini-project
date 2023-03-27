3a435e2fb2e61c124c7a590171695497870b723f
George Samuel <geosam2708@gmail.com>
2023-03-27 04:49:42 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  //add task
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let value = document.getElementById('new-task-description').value
+    let li = document.createElement('li')
+    li.innerText = value
+    let btn = document.createElement('button')
+    btn.innerText = " x"
+    btn.id = "button"
+    btn.style.marginLeft = "10%";
+    li.appendChild(btn)
+    document.getElementById('tasks').appendChild(li)
+
+    //delete task
+    btn.addEventListener('click', (e) => {
+      btn.parentElement.remove()
+    })
+
+    
+    
+  })
 });
